d486c4d0a40e5b8b9e8faed527308f5135658124
roboticsclub1610-prog <roboticsclub1610@gmail.com>
2025-11-28 10:50:34 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyAMBcb42DCrajz9VNFKXBtoeWDrMlZ9CwE",
   authDomain: "exhibition-d2a2a.firebaseapp.com",
   projectId: "exhibition-d2a2a",
-  storageBucket: "exhibition-d2a2a.appspot.com",
+  storageBucket: "exhibition-d2a2a.firebasestorage.app",
   messagingSenderId: "147308882686",
   appId: "1:147308882686:web:232bc99e4b7d6ab3e493af",
   measurementId: "G-88SB65JLPS"
